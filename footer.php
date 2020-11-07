db04196d6ce600a1a45a706f9663cb00d09435e2
regine-tiu <regine.d.tiu@gmail.com>
2020-11-06 22:46:24 -0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "wrapper" div and all content after.
+ *
+ * @package Neve
+ * @since   1.0.0
+ */
+
+do_action( 'neve_before_primary_end' );
+?>
+</main><!--/.neve-main-->
+
+</div><!--/.wrapper-->
+
+<footer>
+	<div class="social-icons">
+		<ul>
+			<li><a href="#"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/twitter-icon.png"></a></li>
+			<li><a href="#"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/linkedin-icon.png"></a></li>
+			<li><a href="#"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/ec-icon.png"></a></li>
+			<li><a href="#"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/mail-icon.png"></a></li>
+	</div>
+	<p> Copyright Rookwood Editing 2020 &#124; Designed by <a href="#"> Reggie B.</a></p>
+</footer>
+</body>
+
+</html>
