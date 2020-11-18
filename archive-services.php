b68f7e70f300f863f0b225bdf6e7eeab4e8ef652
regine-tiu <regine.d.tiu@gmail.com>
2020-11-18 12:11:04 -0500

    Service imgs showing up

--- archive-services.php
+++ archive-services.php
@@ -8,8 +8,8 @@
  * different template.
  *
  * @package WordPress
- * @subpackage Accelerate Marketing
- * @since Accelerate Marketing 2.0
+ * @subpackage Rookwood Editing
+ * @since Rookwood Editing 2020
  */
 
 get_header(); ?>
@@ -17,19 +17,21 @@ get_header(); ?>
 <section class="page">
 	<div class="row">
       <?php 
-					$intro = get_field('service-page-intro'); ?> 
+					$intro = get_field("service_page_intro"); ?> 
         <?php echo $intro ; ?> 
 
 		  <?php while ( have_posts() ) : the_post(); 
-      			$service_img = get_field("service-image");
-						$size = "medium";?>
+      			$image = get_field("service_image");
+            $size = "medium"; ?>
+            
         <article class="services-item">
           <div class="service-item-images">
-						<?php echo wp_get_attachment_image($service_img, $size); ?>
+              <?php echo wp_get_attachment_image( $image, $size ); ?>
           </div>
 
           <aside class="service-item-text">
             <h3><?php the_title(); ?></h3>
+            
             <?php the_content(); ?>
           </aside>  
         </article>	
