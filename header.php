b9b2857d3ed643d88ad7b7a501bf66cc457ad3c0
regine-tiu <regine.d.tiu@gmail.com>
2020-11-26 18:06:28 -0500

    nav bar complete

--- header.php
+++ header.php
@@ -16,7 +16,7 @@ $header_classes = apply_filters( 'nv_header_classes', 'header' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	
+	<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@400;500;600;700&display=swap" rel="stylesheet">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
@@ -28,23 +28,22 @@ $header_classes = apply_filters( 'nv_header_classes', 'header' );
 <div class="wrapper">
 	<?php neve_before_header_wrapper_trigger(); ?>
 	<header class="<?php echo esc_attr( $header_classes ); ?>" role="banner">
-		<div class="header-logo">
+	<div class="header-logo">
 			<img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/header-logo.png">
-		</div>	
-		<?php
-		neve_before_header_trigger();
-		if ( apply_filters( 'neve_filter_toggle_content_parts', true, 'header' ) === true ) {
-			do_action( 'neve_do_header' );
-		}
-		neve_after_header_trigger();
-		?>
+		</div>
+	
+		<div class="nav-bar">
+			<ul class="nav-list">
+				<li><a class="nav-item" href="<?php echo site_url('')?>">Home</a></li>
+				<li><a class="nav-item" href="<?php echo site_url('/about')?>">About</a></li>
+				<li><a class="nav-item" href="<?php echo site_url('/service')?>">Services</a></li>
+				<li><a class="nav-item" href="<?php echo site_url('/testimonial')?>">Testomonials</a></li>
+				<li><a class="nav-item" href="<?php echo site_url('/contact-2')?>">Contact</a></li>
+				<li><a class="nav-item" href="<?php echo site_url('')?>">Blog</a></li>
+			</ul>	
+		</div>
+
 	</header>
-
-	<?php neve_after_header_wrapper_trigger(); ?>
-	<?php do_action( 'neve_before_primary' ); ?>
-
-	<main id="content" class="neve-main" role="main">
-
 <?php
-do_action( 'neve_after_primary_start' );
+
 
