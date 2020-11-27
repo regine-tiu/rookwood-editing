13aa606d242d2854885db780cf359cdf003dd07c
regine-tiu <regine.d.tiu@gmail.com>
2020-11-27 15:29:58 -0500

    about page content

--- archive-about.php
+++ archive-about.php
@@ -17,28 +17,28 @@ get_header(); ?>
 <section class="page">
 	<div class="row">
     <h1>About</h1>
-	   <?php while ( have_posts() ) : the_post(); 
-      	 $about_img = get_field("about_image");
-         $about_summary = get_field("about_summary");
-         $crow_feet_img = get_field("about_page_deco_img");
-         $size = "full"; ?>
-            
+     <?php while ( have_posts() ) : the_post();
+     $about_img = get_field("about_image");
+          $about_summary = get_field("about_summary");
+          $crow_feet_img = get_field("about_page_deco_img");
+          $size = "full"; ?>
+
         <div class="about-page-text">
            <h3><?php the_title(); ?></h3>
             <?php the_content(); ?>
         </div>  
-        
-      <?php endwhile; // end of the loop. ?>
 
-        <div class="about-page-sidebar">
+      <div class="about-page-sidebar">
           <div class="about-image"> 
-            <img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/molly-rookwood-profilepic-e1605805144430.png">
+            <?php echo wp_get_attachment_image($about_img, $size); ?> 
           </div> 
 
           <div class="about-summary">
-             <?php echo $about_summary; ?>
+            <?php	echo $about_summary;?>	
           </div>
-        </div>
+      </div>
+      <?php endwhile; // end of the loop. ?>
+          
 	</div>
 </section>
 
