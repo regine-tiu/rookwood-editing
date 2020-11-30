21f8fc48897a56441e115c34a15e1e9bcf394b11
regine-tiu <regine.d.tiu@gmail.com>
2020-11-30 14:13:48 -0500

    added footer links

--- footer.php
+++ footer.php
@@ -17,10 +17,10 @@ do_action( 'neve_before_primary_end' );
 <footer>
 	<div class="social-icons">
 		<ul>
-			<li><a href="#"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/twitter-icon.png"></a></li>
-			<li><a href="#"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/linkedin-icon.png"></a></li>
-			<li><a href="#"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/ec-icon.png"></a></li>
-			<li><a href="#"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/mail-icon.png"></a></li>
+			<li><a href="https://twitter.com/RookwoodEditing"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/twitter-icon.png"></a></li>
+			<li><a href="https://www.linkedin.com/in/molly-rookwood-108950164/"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/linkedin-icon.png"></a></li>
+			<li><a href="https://www.editors.ca/directory/molly-rookwood"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/ec-icon.png"></a></li>
+			<li><a href="<?php echo site_url('/contact-2')?>"><img src="http://localhost:8888/wordpress/wp-content/uploads/2020/11/mail-icon.png"></a></li>
 	</div>
 	<div class="footer-text">
 		<p> Copyright Rookwood Editing 2020 &#124; Designed by <a href="https://www.instagram.com/matcha_fiend/"> Reggie B.</a></p>
